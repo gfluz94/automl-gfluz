3796184bb6a615262805790e77c7b197a63b0acf
gfluz94 <52358345+gfluz94@users.noreply.github.com>
2020-08-25 18:00:43 -0300

    updating regression plots for analysis

--- automl/regression.py
+++ automl/regression.py
@@ -114,14 +114,17 @@ class RegressorPyCaret(BaseEstimator):
         
         return self.all_models
         
-    def predict(self, X):
+    def predict(self, X, postprocess=None):
         if not self.trained:
             raise ValueError("Model not fitted yet!")
         if not isinstance(X, pd.DataFrame):
             cols = ['X_'+str(i+1) for i in range(X.shape[1])]
             X = pd.DataFrame(X, columns=cols)
         X_transformed = self.preprocessor.transform(X)
-        return self.best_model.predict(X_transformed)
+        output = self.best_model.predict(X_transformed)
+        if postprocess is not None:
+            output = postprocess(output)
+        return output
     
     def preprocess(self, X):
         if not isinstance(X, pd.DataFrame):
@@ -129,42 +132,66 @@ class RegressorPyCaret(BaseEstimator):
             X = pd.DataFrame(X, columns=cols)
         return self.preprocessor.transform(X)
     
-    def evaluate(self, X, y_true):
+    def evaluate(self, X, y_true, postprocess=None):
         if not self.trained:
             raise ValueError("Model not fitted yet!")
         METRICS = ["MAE", "MSE", "RMSE", "R2", "RMLSE", "MAPE"]
         FUNCTIONS = [mean_absolute_error, mean_squared_error, rmse, r2_score, rmlse, mape]
         metrics = {}
-        y_pred = self.predict(X)
+        y_pred = self.predict(X, postprocess=postprocess)
         for metric, function in zip(METRICS, FUNCTIONS):
             metrics[metric] = function(y_true, y_pred)
 
         return pd.DataFrame(metrics, index=[self.__name_model])
     
-    def plot_analysis(self, X, y_true):
-        _, (ax1, ax2) = plt.subplots(1,2,figsize=(16,7))
-        y_pred = self.predict(X)
+    def plot_analysis(self, X, y_true, postprocess=None, name=None):
+        _, (ax1, ax2, ax3, ax4) = plt.subplots(1,4,figsize=(14,3))
+        y_pred = self.predict(X, postprocess=postprocess)
         residuals = y_true - y_pred
-        ax1.set_title("Residuals Plot", fontsize=13)
+
+        if name is not None:
+            txt_to_show = f"{name} | RMSE: {rmse(y_true, y_pred):.3f} | R2: {r2_score(y_true, y_pred):.3f}"
+            print(len(txt_to_show)*'-')
+            print(txt_to_show)
+            print(len(txt_to_show)*'-')
+        ax1.set_title("Histogram", fontsize=13)
+        ax1.hist(y_true, color="lightblue", edgecolor="navy", alpha=1, label="True")
+        ax1.hist(y_pred, color="red", edgecolor="red", alpha=0.6, label="Predicted")
+        ax1.legend()
+
+        ax2.set_title("Residuals Plot", fontsize=13)
         sns.regplot(x=y_pred, y=residuals, ci=0, 
                     scatter_kws={"color":"lightblue", "linewidth":1, "edgecolors":"navy"}, 
-                    line_kws={"color": "red"}, ax=ax1)
-        ax1.plot([min(y_pred), max(y_pred)], [0, 0], color="black", linestyle="--")
-        ax1.set_xlabel("Fitted Values")
-        ax1.set_ylabel("Residuals")
+                    line_kws={"color": "red"}, ax=ax2)
+        ax2.plot([min(y_pred), max(y_pred)], [0, 0], color="black", linestyle="--")
+        ax2.set_xlabel("Fitted Values")
+        ax2.set_ylabel("Residuals")
 
         standardized_residuals = (np.sort(residuals)-np.mean(residuals))/np.std(residuals, ddof=1)
         n = len(standardized_residuals)
-        theoretical_quantiles = [scipy.stats.norm(0, 1).ppf((i+1)/(n+1)) for i in range(n)]
-        ax2.set_title("QQ-Plot", fontsize=13)
+        theoretical_quantiles = [scipy.stats.norm.ppf(p) for p in np.linspace(0.01, 0.99, len(standardized_residuals))]
+        ax3.set_title("QQ-Plot", fontsize=13)
         sns.regplot(x=theoretical_quantiles, y=standardized_residuals, fit_reg=False,
                     scatter_kws={"color":"lightblue", "linewidth":1, "edgecolors":"navy"}, 
-                    line_kws={"color": "red"}, ax=ax2)
-        ax2.plot([min(theoretical_quantiles), max(theoretical_quantiles)],
+                    line_kws={"color": "red"}, ax=ax3)
+        ax3.plot([min(theoretical_quantiles), max(theoretical_quantiles)],
                 [min(theoretical_quantiles), max(theoretical_quantiles)], color="red", linestyle="--")
-        ax2.set_ylabel("Standardized Residuals")
-        ax2.set_xlabel("Theoretical Quantiles")
+        ax3.set_xlim((min(theoretical_quantiles), max(theoretical_quantiles)))
+        ax3.set_ylim((min(theoretical_quantiles), max(theoretical_quantiles)))
+        ax3.set_ylabel("Standardized Residuals")
+        ax3.set_xlabel("Theoretical Quantiles")
 
+        ax4.set_title("Predicted x True", fontsize=13)
+        min_val = min([y_pred.min(), y_true.min()])
+        max_val = max([y_pred.max(), y_true.max()])
+        sns.scatterplot(y_pred, y_true, color="lightblue", edgecolor="navy", ax=ax4)
+        ax4.plot([min_val, max_val], [min_val, max_val], color="red", ls="dashed")
+        ax4.set_xlim([min_val, max_val])
+        ax4.set_ylim([min_val, max_val])
+        ax4.set_xlabel("Predicted Values")
+        ax4.set_ylabel("True Values")
+
+        plt.tight_layout()
         plt.show()
         
     def save_pipeline(self, filename=None):
